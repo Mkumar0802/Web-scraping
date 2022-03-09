142ced9cdd85f88c5a59c85513ce4435a286de69
mkumar0802 <mkumar0802@gmail.com>
2022-03-09 15:31:02 +0530

    link update

--- scraping.js
+++ scraping.js
@@ -7,12 +7,12 @@ const getData = async () => {
   let amazonData = [];
   try {
     const response = await axios.get(
-      `https://www.amazon.in/s?k=mobile&crid=1CVRSXM8Z5F8Q&sprefix=mobi%2Caps%2C461&ref=nb_sb_ss_ts-doa-p_2_4`
+      `https://www.amazon.in/s?k=mobile&crid=2D2EXHAEKZBOE&sprefix=%2Caps%2C306&ref=nb_sb_ss_recent_1_0_recent`
     );
     const $ = cheerio.load(response.data);
 
     $(".s-asin").each((index, ele) => {
-      if (index < 10) {
+      if (index < 15) {
         let image = $(ele).find(".aok-relative").children().attr("src");
         let title = $(ele).find("span.a-text-normal").text();
         let rating = $(ele).find(".a-icon-star-small").children().text();
@@ -33,12 +33,12 @@ const getData = async () => {
   let flipKartData = [];
   try {
     const response = await axios.get(
-      `https://www.flipkart.com/search?q=mobiles&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_1_5_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_5_na_na_na&as-pos=1&as-type=RECENT&suggestionId=mobiles%7CMobiles&requestId=be7e2d79-51ff-4a1f-ba14-d8af4fd615b3&as-searchtext=mobil`
+      `https://www.flipkart.com/search?q=mobiles&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_1_5_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_5_na_na_na&as-pos=1&as-type=HISTORY&suggestionId=mobiles%7CMobiles&requestId=be7e2d79-51ff-4a1f-ba14-d8af4fd615b3`
     );
     const $ = cheerio.load(response.data);
     let count = 0;
     $("._1AtVbE").each((index, ele) => {
-      if (count < 10) {
+      if (count < 15) {
         let image = $(ele).find("._396cs4").attr("src");
         let title = $(ele).find("._4rR01T").text();
         let rating = $(ele).find("span._1lRcqv").children().text();
@@ -64,7 +64,7 @@ const getData = async () => {
     const $ = cheerio.load(response.data);
     let count = 0;
     $(".favDp").each((index, ele) => {
-      if (count < 10) {
+      if (count < 15) {
         let image = $(ele).find(".picture-elem source").attr("srcset");
         let title = $(ele).find(".product-title").text();
         let rating = "NA";
